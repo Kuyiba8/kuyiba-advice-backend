29526e844a0a91f25fcee5bb1a872e1931eef990
Kuyiba Anthony <kuyibanthony88@gmail.com>
2023-09-27 00:37:19 +0000

    modified:   package.json         modified:   server.js

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 require("dotenv").config();
-
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors"); // Import the 'cors' package
 const workoutRoutes = require("./routes/workouts");
 const userRoutes = require("./routes/user");
 
-// express app
 const app = express();
 
 // Database connection
@@ -17,32 +16,21 @@ mongoose
   .then(() => console.log("Database Connected"))
   .catch((err) => console.log("Database Not Connected", err));
 
-// middleware
+// Middleware
 app.use(express.json());
+
+// Enable CORS for all routes (you can also specify origins as needed)
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
-// routes
+// Routes
 app.use("/api/workouts", workoutRoutes);
 app.use("/api/user", userRoutes);
 
-// // connect to db
-// mongoose
-//   .connect(process.env.MONGO_URI)
-//   .then(() => {
-//     // listen for requests
-//     app.listen(process.env.PORT, () => {
-//       console.log("connected to db & listening on port", process.env.PORT);
-//     });
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
-
-//connecting the server
 const port = process.env.PORT || 7000;
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`);
