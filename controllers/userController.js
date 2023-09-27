ad7e56fcae2c2bfa3436536352a523f547ca6ab7
Kuyiba Anthony <kuyibanthony88@gmail.com>
2023-09-27 10:42:22 +0000

    modified:   controllers/userController.js         modified:   models/userModel.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -31,7 +31,7 @@ const signupUser = async (req, res) => {
     // create a token
     const token = createToken(user._id);
 
-    res.status(200).json({ email, token });
+    res.status(200).json({ user, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
